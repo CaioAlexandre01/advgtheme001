6ec47b67b2dbf23d8b73897076a5d53467e56a4d
Caio Alexandre <caioalexandreml@gmail.com>
2024-12-17 22:45:10 -0300

    fixed menu mobile

--- src/App.jsx
+++ src/App.jsx
@@ -38,48 +38,55 @@ function App() {
   return (
     <div className="w-screen text-white">
       <div>
-        <header className="w-screen bg-[#1C2129] text-center fixed z-50">
-          <nav className="bg-transparent text-slate-100 shadow-lg py-3">
-            <div className="container mx-auto px-4 flex items-center justify-between">
-              <img src={logo} alt="Logo" className="rounded-full h-12 bg-transparent" /> {/* Logo com altura reduzida */}
-              <div className="lg:hidden" onClick={toggleMobileMenu}>
-                {isMobileMenuOpen ? <FaTimes className="text-gold text-3xl" /> : <FaBars className="text-gold text-3xl" />}
-              </div>
-              <div className="hidden lg:flex space-x-6">
-                <a href="#especialidades" className="text-gray-400 hover:text-gold">
-                  Especialidades
-                </a>
-                <a href="#testemunhos" className="text-gray-400 hover:text-gold">
-                  Testemunhos
-                </a>
-                <a href="#contato" className="text-gray-400 hover:text-gold">
-                  Contato
-                </a>
-              </div>
-            </div>
-            <motion.div
-              className={`lg:hidden bg-slate-100 rounded-lg py-4 mt-2 shadow-2xl ${isMobileMenuOpen ? "block" : "hidden"}`} // Controle da visibilidade do menu
-              initial={{ opacity: 0, y: 10 }}
-              animate={{
-                opacity: isMobileMenuOpen ? 1 : 0,
-                y: isMobileMenuOpen ? 0 : 10,
-              }}
-              transition={{ duration: 0.5, ease: "easeOut" }}
-            >
-              <div className="text-center">
-                <a href="#testemunhos" className="block text-gray-700 py-2 px-4 hover:text-gold">
-                  Testemunhos
-                </a>
-                <a href="#faq" className="block text-gray-700 py-2 px-4 hover:text-gold">
-                  Perguntas frequentes
-                </a>
-                <a href="#contato" className="block text-gray-700 py-2 px-4 hover:text-gold">
-                  Contato
-                </a>
-              </div>
-            </motion.div>
-          </nav>
-        </header>
+      <header className="w-screen bg-[#1C2129] text-center fixed z-50">
+  <nav className="bg-transparent text-slate-100 shadow-lg py-3">
+    <div className="container mx-auto px-4 flex items-center justify-between">
+      <img src={logo} alt="Logo" className="rounded-full h-12 bg-transparent" /> {/* Logo com altura reduzida */}
+      
+      {/* Botão de menu mobile */}
+      <div className="lg:hidden" onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ? <FaTimes className="text-gold text-3xl" /> : <FaBars className="text-gold text-3xl" />}
+      </div>
+
+      {/* Menu para dispositivos grandes */}
+      <div className="hidden lg:flex space-x-6">
+        <a href="#especialidades" className="text-gray-400 hover:text-gold" onClick={() => setIsMobileMenuOpen(false)}>
+          Especialidades
+        </a>
+        <a href="#testemunhos" className="text-gray-400 hover:text-gold" onClick={() => setIsMobileMenuOpen(false)}>
+          Testemunhos
+        </a>
+        <a href="#contato" className="text-gray-400 hover:text-gold" onClick={() => setIsMobileMenuOpen(false)}>
+          Contato
+        </a>
+      </div>
+    </div>
+
+    {/* Menu mobile */}
+    <motion.div
+      className={`lg:hidden bg-slate-100 rounded-lg py-4 mt-2 shadow-2xl ${isMobileMenuOpen ? "block" : "hidden"}`} // Controle da visibilidade do menu
+      initial={{ opacity: 0, y: 10 }}
+      animate={{
+        opacity: isMobileMenuOpen ? 1 : 0,
+        y: isMobileMenuOpen ? 0 : 10,
+      }}
+      transition={{ duration: 0.5, ease: "easeOut" }}
+    >
+      <div className="text-center">
+        <a href="#testemunhos" className="block text-gray-700 py-2 px-4 hover:text-gold" onClick={() => setIsMobileMenuOpen(false)}>
+          Testemunhos
+        </a>
+        <a href="#faq" className="block text-gray-700 py-2 px-4 hover:text-gold" onClick={() => setIsMobileMenuOpen(false)}>
+          Perguntas frequentes
+        </a>
+        <a href="#contato" className="block text-gray-700 py-2 px-4 hover:text-gold" onClick={() => setIsMobileMenuOpen(false)}>
+          Contato
+        </a>
+      </div>
+    </motion.div>
+  </nav>
+</header>
+
 
         <div className="bg-cover bg-center h-auto relative" style={{ backgroundImage: `url('/background1.jpg')` }}>
           <div className="flex flex-col items-center justify-center text-center h-full bg-black bg-opacity-20">
